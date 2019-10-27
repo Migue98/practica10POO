979add26d8731bbc92e90b08694c202b8e5d40b3
shenshuais <42390833+shenshuais@users.noreply.github.com>
2019-10-27 14:06:23 -0600

    ejercicio 5

---
+++ TraceException.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package traceexception;
+
+/**
+ *
+ * @author shens
+ */
+public class TraceException {
+
+    public static void main (String[] args){
+		try{
+			metodo1();
+		}catch(Exception excepcion){
+			System.err.println(excepcion.getMessage() + "\n");
+			StackTraceElement[] elementosRastreo = excepcion.getStackTrace();
+
+			System.out.println("\nRastreo de pila proveniente de getStacTrace:");
+			System.out.println("Clase\t\tArchivo\t\t\tLinea\tMetodo");
+
+			for(int i = 0; i < elementosRastreo.length; i++){
+				StackTraceElement elementoActual = elementosRastreo[i];
+				System.out.print(elementoActual.getClassName() + "\t");
+				System.out.print(elementoActual.getFileName() + "\t");
+				System.out.print(elementoActual.getLineNumber() + "\t");
+				System.out.print(elementoActual.getMethodName() + "\n");
+			}
+		}
+	}
+
+	public static void metodo1() throws Exception{
+		metodo2();
+	}
+
+	public static void metodo2() throws Exception{
+		metodo3();
+	} 
+
+	public static void metodo3() throws Exception{
+		throw new Exception("La excepcion se lanzo en metodo3");
+	}
+    
+}
